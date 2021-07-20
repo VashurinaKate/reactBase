0751aa85194e5a31635a1c4ab6a4d24783d84ee1
KateVashurina <wasturina.ekatterina@gmail.com>
2021-07-20 22:25:44 +0300

    task1_part3

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App(props) {
   console.log(props)
+  const message = "This is message from props";
   return (
     <div className="App">
       <header className="App-header">
         My First React App
-        <Message msg="This is message from props"/>
+        <Message msg={message}/>
       </header>
     </div>
   );
